682311d5489d335fc299180c7daaab90cc966f25
Tim Graboski <tgraboski@tutela.com>
2024-08-20 19:11:03 -0700

    GL Context with resizing canvas and animation loop

---
+++ src/AnimationLoop.ts
@@ -0,0 +1,112 @@
+export interface ITimestamp {
+  /** Age in milliseconds */
+  age: number;
+
+  /** Time in milliseconds */
+  now: number;
+
+  /** time since previous frame in milliseconds */
+  deltaT: number;
+}
+
+type RenderCallback = (timestamp: ITimestamp) => void;
+export class AnimationLoop {
+  private renderCallback: RenderCallback;
+  private pausedAt: number | undefined;
+  private startTime: number;
+  private previousTime: number;
+
+  constructor(callback: RenderCallback) {
+    this.renderCallback = callback;
+
+    const now = performance.now();
+    this.pausedAt = now;
+    this.startTime = now;
+    this.previousTime = now - 16.7; // initial frame length: 1/60th of a second
+  }
+
+  public resume = (renderCallback?: RenderCallback) => {
+    if (this.pausedAt === undefined) {
+      return;
+    }
+
+    if (renderCallback) {
+      this.renderCallback = renderCallback;
+    }
+
+    const now = performance.now();
+    const pauseDelay = now - this.pausedAt;
+    this.startTime += pauseDelay;
+    this.previousTime += pauseDelay;
+    this.pausedAt = undefined;
+    requestAnimationFrame(this.render);
+  };
+
+  public render = () => {
+    if (this.pausedAt !== undefined) {
+      return;
+    }
+
+    const now = performance.now();
+    const age = now - this.startTime;
+    const deltaT = now - this.previousTime;
+    this.previousTime = now;
+
+    const timestamp = {
+      now,
+      age,
+      deltaT,
+    };
+
+    this.renderCallback(timestamp);
+    requestAnimationFrame(this.render);
+  };
+
+  public pause = () => {
+    this.pausedAt = performance.now();
+  };
+
+  public toggle = () => {
+    if (this.pausedAt !== undefined) {
+      this.resume();
+    } else {
+      this.pause();
+    }
+  };
+
+  public step = () => {
+    if (this.getIsPaused()){
+      const now = performance.now();
+      const age = 16;
+      const deltaT = 16;
+
+      const timestamp = {
+        now,
+        age,
+        deltaT,
+      };
+  
+      this.renderCallback(timestamp);
+    }
+  }
+
+  public attachSpacebarToggle = () => {
+    document.addEventListener("keypress", (event) => {
+      if (event.repeat) {
+        return;
+      }
+      if (event.key == " ") {
+        this.toggle();
+        return;
+      }
+      if (event.key === "]"){
+        if (this.getIsPaused()) {
+          this.step();
+        }
+        return;
+      }
+    });
+  }
+
+  public getIsPaused = () => this.pausedAt !== undefined;
+}
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,32 @@
+import { AnimationLoop, ITimestamp } from "./AnimationLoop";
 import "./assets/style.css";
+import { GLContext } from "./GLContext";
 
-console.log("Hello, World!");
+let mustResize = false;
+const main = () => {
+  const canvas = document.getElementById("glcanvas") as HTMLCanvasElement;
+  const glContext = new GLContext(canvas);
+  const { gl } = glContext;
+
+  window.addEventListener("resize", () => {
+    mustResize = true;
+  });
+
+  const looper = new AnimationLoop((timestamp: ITimestamp) => {
+    if (mustResize){
+      glContext.resize(window.innerWidth, window.innerHeight);
+      mustResize = false;
+    }
+
+    gl.viewport(0, 0, glContext.width, glContext.height);
+    gl.clearColor(0.8745, 0.8745, 0.8745, 1.0); // Clear to light grey, fully opaque
+    gl.clearDepth(1.0); // Clear everything
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+    gl.disable(gl.DEPTH_TEST); // Disable depth testing
+  });
+
+  looper.attachSpacebarToggle();
+  looper.resume();
+}
+
+main();
